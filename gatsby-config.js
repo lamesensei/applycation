d77cb6a9e6d465ae0b14f0a686dd029885baba73
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-11-15 13:27:47 +0800

    Add graphql source plugin and successfully test connection

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
+require('dotenv').config()
+
+const fetch = require(`node-fetch`)
+const { createHttpLink } = require(`apollo-link-http`)
+
 module.exports = {
+  pathPrefix: '/applycation',
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
@@ -23,6 +29,22 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-source-graphql', // <- Configure plugin
+      options: {
+        typeName: 'HASURA',
+        fieldName: 'hasura', // <- fieldName under which schema will be stitched
+        createLink: () =>
+          createHttpLink({
+            uri: `${process.env.HASURA_GRAPHQL_URL}`, // <- Configure connection GraphQL url
+            headers: {
+              'X-Hasura-Access-Key': `${process.env.HASURA_GRAPHQL_ACCESS_KEY}`,
+            },
+            fetch,
+          }),
+        refetchInterval: 10, // Refresh every 10 seconds for new data
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,28 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>{console.log(data.hasura.first)}</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query Query {
+    hasura {
+      first: test(where: { id: { _eq: 1 } }) {
+        id
+        name
+      }
+    }
+  }
+`
 
 export default IndexPage
