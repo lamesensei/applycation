90f94ff08c6df34c452348233a9bf8b78a1d6e0b
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-11-20 10:34:30 +0800

    Change statepanel to card, add taskitem

--- src/components/stage/StagePanel.js
+++ src/components/stage/StagePanel.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardFooter,
+  Button,
+  ListGroup
+} from 'reactstrap';
+
 import Stage from '../functions/stage';
+import TaskItem from '../task/TaskItem';
 
 class StagePanel extends Component {
   clickHandler = () => {
@@ -8,13 +20,27 @@ class StagePanel extends Component {
   };
   render() {
     return (
-      <div className="d-flex justify-content-between align-items-center border p-2 mb-2">
-        <h3>{this.props.name}</h3>
-        <span>{this.props.notes}</span>
-        <Button size="sm" color="danger" onClick={this.clickHandler}>
-          <i className="fas fa-trash-alt" />
-        </Button>
-      </div>
+      <Card className="mb-2">
+        {/* <CardImg
+          top
+          width="100%"
+          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+          alt="Card image cap"
+        /> */}
+        <CardBody>
+          <CardTitle>{this.props.name}</CardTitle>
+          <CardSubtitle>due date here</CardSubtitle>
+          <CardText>{this.props.notes}</CardText>
+        </CardBody>
+        <ListGroup flush>
+          <TaskItem />
+        </ListGroup>
+        <CardFooter className="text-muted">
+          <Button className="float-right" size="sm" color="danger" onClick={this.clickHandler}>
+            <i className="fas fa-trash-alt" />
+          </Button>
+        </CardFooter>
+      </Card>
     );
   }
 }
---
+++ src/components/task/TaskItem.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { ListGroupItem } from 'reactstrap';
+
+class TaskItem extends Component {
+  render() {
+    return <ListGroupItem>test</ListGroupItem>;
+  }
+}
+
+export default TaskItem;
