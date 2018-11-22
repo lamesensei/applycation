4f974cf7f1d03e54c1f1db6fdaecf07dfc9f48ea
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-11-22 23:48:25 +0800

    Add signature

--- src/components/nav/Left.js
+++ src/components/nav/Left.js
@@ -5,17 +5,22 @@ import { Nav } from 'reactstrap';
 class Left extends Component {
   render() {
     return (
-      <Nav className="mt-3" vertical>
-        <Link className="nav-link nav-item text-white" to="/profile" data-toggle="pills">
-          <strong>Profile</strong>
-        </Link>
-        <Link className="nav-link nav-item text-white" to="/job/apply" data-toggle="pills">
-          <strong>Apply</strong>
-        </Link>
-        <Link className="nav-link nav-item text-white" to="/jobs" data-toggle="pills">
-          <strong>Applications</strong>
-        </Link>
-      </Nav>
+      <div className="h-100 d-flex flex-column justify-content-between align-items-between">
+        <Nav className="mt-3" vertical>
+          <Link className="nav-link nav-item text-white" to="/profile" data-toggle="pills">
+            <strong>Profile</strong>
+          </Link>
+          <Link className="nav-link nav-item text-white" to="/job/apply" data-toggle="pills">
+            <strong>Apply</strong>
+          </Link>
+          <Link className="nav-link nav-item text-white" to="/jobs" data-toggle="pills">
+            <strong>Applications</strong>
+          </Link>
+        </Nav>
+        <p className="pl-4 pr-4 text-white">
+          made by <strong>LJ</strong>
+        </p>
+      </div>
     );
   }
 }
