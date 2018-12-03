dd79705938b5911304736588f7cdfe49331d48f3
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-12-03 15:06:18 +0800

    Add splitting of active/inactive jobs

--- src/components/job/JobList.js
+++ src/components/job/JobList.js
@@ -4,6 +4,7 @@ import { UncontrolledAlert } from 'reactstrap';
 import { Badge, MDBRow } from 'mdbreact';
 import JobItem from './JobItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import moment from 'moment';
 
 class JobList extends Component {
   constructor() {
@@ -11,19 +12,36 @@ class JobList extends Component {
     this.state = {
       isEmpty: false,
       jobs: [],
+      inactiveJobs: [],
       jobDeleted: ''
     };
   }
 
   populateJobs = (data, isEmpty) => {
-    if (!isEmpty)
-      this.setState({
-        jobs: [...data],
-        isEmpty: false
+    console.log(data);
+    let activeJobs = [];
+    let inactiveJobs = [];
+    if (!isEmpty) {
+      data.forEach((item) => {
+        let dueDate = '';
+        let currentDate = moment();
+        if (item.stages.length > 0) {
+          dueDate = moment(item.stages[0].due, moment.ISO_8601);
+          if (parseInt(currentDate.diff(dueDate, 'days')) > 13) inactiveJobs.push(item);
+          else activeJobs.push(item);
+        } else {
+          activeJobs.push(item);
+        }
       });
-    else
+      this.setState({
+        jobs: [...activeJobs],
+        inactiveJobs: [...inactiveJobs],
+        isEmpty: activeJobs.length > 1 ? false : true
+      });
+    } else
       this.setState({
         jobs: [],
+        inactiveJobs: [],
         isEmpty: true
       });
   };
@@ -41,6 +59,19 @@ class JobList extends Component {
 
   render() {
     const jobs = this.state.jobs.map((item) => {
+      return (
+        <JobItem
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          company={item.company.name}
+          job={item}
+          deleteHandler={this.deleteHandler}
+        />
+      );
+    });
+
+    const inactiveJobs = this.state.inactiveJobs.map((item) => {
       return (
         <JobItem
           key={item.id}
@@ -73,6 +104,11 @@ class JobList extends Component {
           </h1>
           {loading}
           <MDBRow>{jobs}</MDBRow>
+          <hr />
+          <h1>
+            Inactive Applications <Badge color="red">{this.state.inactiveJobs.length}</Badge>
+          </h1>
+          <MDBRow>{inactiveJobs}</MDBRow>
         </div>
       </React.Fragment>
     );
